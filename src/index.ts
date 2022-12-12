ee5f1b09e60ffe28bb9c29ff763bd37af932a194
Yuriy Galanter <50464505+YuriGal@users.noreply.github.com>
2022-12-12 15:52:39 -0500

    Refactored

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,65 @@
-import generator, { Entity, Response } from 'megalodon';
+import generator from 'megalodon';
 import fs from 'fs';
 import { parse } from 'csv-parse/sync'
 
 const BASE_URL: string = 'https://botsin.space'
 const access_token: string = process.env.MASTO_TOKEN!
 
-const client = generator('mastodon', BASE_URL, access_token)
+const mastodon = generator('mastodon', BASE_URL, access_token)
 
-class MyArray extends Array<string> {
-    lyrics?: string[];
+class Song {
+    readonly album: string;
+    readonly title: string;
+    readonly lyrics: string[]
+
+    constructor(rawSong: string[]) {
+        this.album = rawSong[0];
+        this.title = rawSong[1];
+        this.lyrics = rawSong[3].replace('\n\n', '\n').split('\n').filter(line => line.trim().length > 20); // removing small lines
+    }
+}
+
+function sanitize(str: string): string {
+    return str.replace(/[` .,-?'’()]/g, '')
 }
 
 
 function loadSongs() {
-    const csv: MyArray[] = parse(fs.readFileSync('./pink_floyd_lyrics.csv', 'utf-8'));
-    const songs = csv.filter(song => song[3].trim() !== '') // removing songs with small lyrics
-    songs.sort(() => 0.5 - Math.random()) // shuffling songs
-
-    // splitting songs into lines
-    for (let song of songs) {
-        song.lyrics = song[3].replace('\n\n', '\n').split('\n').filter(line => line.trim().length > 20); // removing small lines
-        song.lyrics.sort((a, b) => 0.5 - Math.random()) // shuffling lines
-    }
+    const csv: string[][] = parse(fs.readFileSync('./pink_floyd_lyrics.csv', 'utf-8')); // reading and parsing CSV file
+    const rawSongs = csv.filter(row => row[3].trim() != '') // removing songs with empty lyrics (instrumentals)
+    const songs = rawSongs.map(rawSong => new Song(rawSong))
 
     return songs
 }
 
-loadSongs();
 
-
-function doQuote() {
+function doQuote(songs: Song[]) {
 
     const songIndex = Math.floor(Math.random() * songs.length)
     const song = songs[songIndex];
 
-    if (song.lyrics!.length !== 0) {
+    if (song.lyrics.length != 0) {
 
-        const lineIndex = Math.floor(Math.random() * song.lyrics!.length)
-        const line = song.lyrics![lineIndex];
+        const lineIndex = Math.floor(Math.random() * song.lyrics.length)
+        const line = song.lyrics[lineIndex];
 
-        const toot = line + '\n\n' +
-            '#' + song[1].replace(/[` .,-?!'’()]/g, '') + ' ' +
-            '#' + song[0].replace(/[` .,-?!'’()]/g, '') + ' ' +
-            '#PinkFloyd'
+        const toot = line + '\n\n' + '#' + sanitize(song.title) + ' ' + '#' + sanitize(song.album) + ' ' + '#PinkFloyd'
 
         console.log(`\n${(new Date()).toLocaleString()}: ${toot}`);
 
-        client.postStatus(toot)
+        mastodon.postStatus(toot)
             .catch(err => console.error(err))
 
-        song.lyrics!.splice(lineIndex, 1)
-        console.log(`- ${song.lyrics!.length} lyric lines remaining in this song`)
+        song.lyrics.splice(lineIndex, 1)
+        console.log(`- ${song.lyrics.length} lyric lines remaining in this song`)
     }
 
-    if (song.lyrics!.length === 0) {
+    if (song.lyrics.length === 0) {
         songs.splice(songIndex, 1);
         console.log(`- removing song with empty lyrics, ${songs.length} songs remaining`)
-
-        if (songs.length === 0) {
-            console.log(`- reloading songs`)
-            songs = loadSongs()
-        }
     }
+
+    return songs.length
 
 }
 
@@ -69,9 +67,15 @@ console.log(`${(new Date()).toLocaleString()}: ****** Floyd Quoter Started *****
 
 let songs = loadSongs()
 
-doQuote();
+doQuote(songs);
 
-setInterval(doQuote, 60 * 60 * 1000); // 1 hour
+setInterval(() => {
+    const sonCount = doQuote(songs);
+    if (sonCount === 0) {
+        console.log(`- reloading songs`)
+        songs = loadSongs()
+    }
+}, 60 * 60 * 1000); // 1 hour
 
 // Mastodon Verification
 // <a rel="me" href="https://botsin.space/@PinkFloydHourly">Mastodon</a>
